7844fb6d5e101e9b3ebb4f20ef780355d8d78fec
KaraKala1427 <ernarerbol027@gmail.com>
2021-10-25 12:22:06 +0600

    crud

---
+++ app/Http/Controllers/Admin/ArticleCrudController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\ArticleRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class ArticleCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class ArticleCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation ;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CloneOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\BulkCloneOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\BulkDeleteOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     *
+     * @return void
+     */
+    public function setup()
+    {
+        $this->crud->setModel(\App\Models\Article::class);
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/article');
+        $this->crud->setEntityNameStrings('article', 'articles');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     *
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        $this->crud->column('category_id');
+        $this->crud->column('title');
+        $this->crud->column('slug');
+        $this->crud->column('tags')->type('relationship');
+        $this->crud->column('image');
+        $this->crud->column('status');
+        $this->crud->column('date');
+        $this->crud->column('featured');
+
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - $this->crud->column('price')->type('number');
+         * - $this->crud->addColumn(['name' => 'price', 'type' => 'number']);
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        $this->crud->setValidation(ArticleRequest::class);
+        $this->crud->field('category_id');
+        $this->crud->field('title')->size(6);
+        $this->crud->field('slug')->size(6);
+        $this->crud->field('content')->type('ckeditor');
+        $this->crud->field('image');
+        $this->crud->field('status')->type('enum');
+        $this->crud->field('date');
+        $this->crud->field('featured');
+        $this->crud->field ('tags')->type('relationship');
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - $this->crud->field('price')->type('number');
+         * - $this->crud->addField(['name' => 'price', 'type' => 'number']));
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->crud->setValidation(ArticleRequest::class);
+
+        $this->setupCreateOperation();
+    }
+}
